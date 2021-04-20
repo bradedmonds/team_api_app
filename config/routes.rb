601cd7b5f4ce04c0be95f3f4834c3c8227cf2f13
Brad Edmonds <bcedmonds0417@gmail.com>
2021-04-19 21:16:34 -0500

    make education contraller/actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+
+    # Education Routes #
+    get "/educations" => "educations#index"
+    get "/educations/:id" => "educations#show"
+    post "/educations" => "educations#create"
+    patch "/educations/id" => "educations#update"
+    delete "/educations/id" => "educations#destroy"
+  end
 end
