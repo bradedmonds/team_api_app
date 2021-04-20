7520f52772d4964fe44188959537c83a95eb5b74
Brad Edmonds <bcedmonds0417@gmail.com>
2021-04-20 18:20:40 -0500

    add routes for skills and capstones

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,19 @@ Rails.application.routes.draw do
     patch "/experiences/:id" => "experiences#update"
     delete "/experiences/:id" => "experiences#destroy"
 
+    #skills
+    get "/skills" => "skills#index"
+    post "/skills" => "skills#create"
+    get "/skills/:id" => "skills#show"
+    patch "/skills/:id" => "skills#update"
+    delete "/skills/:id" => "skills#destroy"
+
+    #capstones
+    get "/capstones" => "capstones#index"
+    post "/capstones" => "capstones#create"
+    get "/capstones/:id" => "capstones#show"
+    patch "/capstones/:id" => "capstones#update"
+    delete "/capstones/:id" => "capstones#destroy"
+    
   end
 end
