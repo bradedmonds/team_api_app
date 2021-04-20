4d795ed36cfc300f761eed2bc7925b3939ade05a
Adam <aswilson14@gmail.com>
2021-04-19 22:09:52 -0400

    complete students crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/students" => "students#index"
+    post "/students" => "students#create"
+    get "/students/:id" => "students#show"
+    patch "/students/:id" => "students#update"
+    delete "/students/:id" => "students#destroy"
+  end
 end
