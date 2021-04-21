357da4c17ece6e68a64bf915db1a793233ca1492
Adam <aswilson14@gmail.com>
2021-04-20 20:06:45 -0400

    add skills routes, views, and crud actions

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,11 @@ Rails.application.routes.draw do
     get "/experiences/:id" => "experiences#show"
     patch "/experiences/:id" => "experiences#update"
     delete "/experiences/:id" => "experiences#destroy"
+
+    get "/skills" => "skills#index"
+    post "/skills" => "skills#create"
+    get "/skills/:id" => "skills#show"
+    patch "/skills/:id" => "skills#update"
+    delete "/skills/:id" => "skills#destroy"
   end
 end
