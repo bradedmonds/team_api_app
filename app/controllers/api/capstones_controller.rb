8d380cfafeab7dec0a3d6697562463bd0afba089
Adam <aswilson14@gmail.com>
2021-04-20 20:12:46 -0400

    fix routes file

--- app/controllers/api/capstones_controller.rb
+++ app/controllers/api/capstones_controller.rb
@@ -1,2 +1,50 @@
 class Api::CapstonesController < ApplicationController
+  def index
+    @capstones = Capstone.all
+    render "index.json.jb"
+  end
+
+  def create
+    @capstone = capstone.new(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details],
+      student_id: params[:student_id],
+    )
+
+    if @capstone.save
+      render "show.json.jb"
+    else
+      render json: { errors: @capstone.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @capstone = Capstone.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @capstone = Capstone.find(params[:id])
+
+    @capstone.start_date = params[:start_date] || capstone.start_date,
+                           @capstone.end_date = params[:end_date] || capstone.end_date,
+                           @capstone.job_title = params[:job_title] || capstone.job_title,
+                           @capstone.company_name = params[:company_name] || capstone.company_name,
+                           @capstone.details = params[:details] || capstone.details,
+                           @capstone.student_id = params[:student_id] || capstone.student_id,
+    if @capstone.save
+      render "show.json.jb"
+    else
+      render json: { errors: @capstone.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    capstone = capstone.find(params[:id])
+    capstone.destroy
+    render json: { message: "Successfully Destroyed Experience" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
+
+    # Education Routes #
+    get "/educations" => "educations#index"
+    get "/educations/:id" => "educations#show"
+    post "/educations" => "educations#create"
+    patch "/educations/id" => "educations#update"
+    delete "/educations/id" => "educations#destroy"
+
     #students
     get "/students" => "students#index"
     post "/students" => "students#create"
@@ -8,16 +16,30 @@ Rails.application.routes.draw do
     delete "/students/:id" => "students#destroy"
  
     #experiences
+
     get "/experiences" => "experiences#index"
     post "/experiences" => "experiences#create"
     get "/experiences/:id" => "experiences#show"
     patch "/experiences/:id" => "experiences#update"
     delete "/experiences/:id" => "experiences#destroy"
 
+
+    #skills
+
     get "/skills" => "skills#index"
     post "/skills" => "skills#create"
     get "/skills/:id" => "skills#show"
     patch "/skills/:id" => "skills#update"
     delete "/skills/:id" => "skills#destroy"
+
+
+    #capstones
+    get "/capstones" => "capstones#index"
+    post "/capstones" => "capstones#create"
+    get "/capstones/:id" => "capstones#show"
+    patch "/capstones/:id" => "capstones#update"
+    delete "/capstones/:id" => "capstones#destroy"
+    
+
   end
 end
